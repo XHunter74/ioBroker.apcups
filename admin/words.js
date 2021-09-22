3bcf385ae8353866246982edbd82c498c1d2e5b2
Serhiy Krasovskyy <xhunter74@gmail.com>
2021-09-22 10:59:18 +0300

    Changed settings translation.

--- admin/words.js
+++ admin/words.js
@@ -2,20 +2,16 @@
 'use strict';
 
 systemDictionary = {
-    'apcups adapter settings': {
-        'en': 'Adapter settings for APCUPS',
-        'ru': 'Настройки адаптера для APCUPS'
-    },
     'upsip': {
-        'en': 'Host address of apcupsd',
-        'ru': 'Адрес хоста apcupsd',
+        'en': 'Host address of apcupsd:',
+        'ru': 'Адрес хоста apcupsd:',
     },
     'upsport': {
-        'en': 'Port of apcupsd',
-        'ru': 'Порт apcupsd'
+        'en': 'Port of apcupsd:',
+        'ru': 'Порт apcupsd:'
     },
     'pollingInterval': {
-        'en': 'Polling interval',
-        'ru': 'Интервал опроса'
+        'en': 'Polling interval:',
+        'ru': 'Интервал опроса:'
     }
 };
