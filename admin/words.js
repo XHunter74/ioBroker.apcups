ac16e8c12cf0383c21568bfff9b03a031c3184d6
Serhiy Krasovskyy <xhunter74@gmail.com>
2021-09-22 23:04:39 +0300

    Fixed checker warnings

--- admin/words.js
+++ admin/words.js
@@ -4,14 +4,17 @@
 systemDictionary = {
     'upsip': {
         'en': 'Host address of apcupsd:',
+        'de': 'Hostadresse von appcupsd:',
         'ru': 'Адрес хоста apcupsd:',
     },
     'upsport': {
         'en': 'Port of apcupsd:',
+        'de': 'Port von appupsd:',
         'ru': 'Порт apcupsd:'
     },
     'pollingInterval': {
         'en': 'Polling interval:',
+        'de': 'Abrufintervall:',
         'ru': 'Интервал опроса:'
     }
 };
