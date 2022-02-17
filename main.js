e42638d43aac713502dc57d7e7084b8751955276
Serhiy Krasovskyy <xhunter74@gmail.com>
2022-02-17 19:47:11 +0200

    [skip ci] Cleanup

--- main.js
+++ main.js
@@ -32,9 +32,6 @@ class ApcUpsAdapter extends utils.Adapter {
             name: 'apcups',
         });
         this.on('ready', this.onReady.bind(this));
-        this.on('stateChange', this.onStateChange.bind(this));
-        // this.on('objectChange', this.onObjectChange.bind(this));
-        // this.on('message', this.onMessage.bind(this));
         this.on('unload', this.onUnload.bind(this));
     }
 
@@ -42,7 +39,7 @@ class ApcUpsAdapter extends utils.Adapter {
      * Is called when databases are connected and adapter received configuration.
      */
     async onReady() {
-        this.log.info(`APCUPCSD: ${this.config.upsip}:${this.config.upsport}`);
+        this.log.info(`Apcupcd location: ${this.config.upsip}:${this.config.upsport}`);
         this.log.info('Polling interval: ' + this.config.pollingInterval);
 
         await this.startPooling();
@@ -66,7 +63,7 @@ class ApcUpsAdapter extends utils.Adapter {
         const ApcAccess = require('./apcaccess');
 
         this.apcAccess = new ApcAccess();
-        this.apcAccess.on('error', async() => {
+        this.apcAccess.on('error', async () => {
             await this.reconnect();
         });
         this.apcAccess.on('connect', () => {
@@ -82,8 +79,8 @@ class ApcUpsAdapter extends utils.Adapter {
         if (this.apcAccess.isConnected === false) {
             try {
                 await this.apcAccess.connect(this.config.upsip, this.config.upsport);
-            } catch (error) {
-                this.log.error(error);
+                // eslint-disable-next-line no-empty
+            } catch {
             }
         }
         if (this.config.pollingInterval > SocketTimeout) {
@@ -106,8 +103,8 @@ class ApcUpsAdapter extends utils.Adapter {
             }
             await client.ping();
             this.log.debug(`Ping apcupsd ${this.config.upsip}:${this.config.upsport}`);
-        } catch (error) {
-            this.log.error(error);
+            // eslint-disable-next-line no-empty
+        } catch {
         }
     }
 
@@ -213,7 +210,7 @@ class ApcUpsAdapter extends utils.Adapter {
     toIsoString(date) {
         const tzo = -date.getTimezoneOffset(),
             dif = tzo >= 0 ? '+' : '-',
-            pad = function(num) {
+            pad = function (num) {
                 const norm = Math.floor(Math.abs(num));
                 return (norm < 10 ? '0' : '') + norm;
             };
@@ -248,57 +245,6 @@ class ApcUpsAdapter extends utils.Adapter {
             callback();
         }
     }
-
-    // If you need to react to object changes, uncomment the following block and the corresponding line in the constructor.
-    // You also need to subscribe to the objects with `this.subscribeObjects`, similar to `this.subscribeStates`.
-    // /**
-    //  * Is called if a subscribed object changes
-    //  * @param {string} id
-    //  * @param {ioBroker.Object | null | undefined} obj
-    //  */
-    // onObjectChange(id, obj) {
-    //     if (obj) {
-    //         // The object was changed
-    //         this.log.info(`object ${id} changed: ${JSON.stringify(obj)}`);
-    //     } else {
-    //         // The object was deleted
-    //         this.log.info(`object ${id} deleted`);
-    //     }
-    // }
-
-    /**
-     * Is called if a subscribed state changes
-     * @param {string} id
-     * @param {ioBroker.State | null | undefined} state
-     */
-    onStateChange(id, state) {
-        if (state) {
-            // The state was changed
-            this.log.info(`state ${id} changed: ${state.val} (ack = ${state.ack})`);
-        } else {
-            // The state was deleted
-            this.log.info(`state ${id} deleted`);
-        }
-    }
-
-    // If you need to accept messages in your adapter, uncomment the following block and the corresponding line in the constructor.
-    // /**
-    //  * Some message was sent to this instance over message box. Used by email, pushover, text2speech, ...
-    //  * Using this method requires "common.messagebox" property to be set to true in io-package.json
-    //  * @param {ioBroker.Message} obj
-    //  */
-    // onMessage(obj) {
-    //     if (typeof obj === 'object' && obj.message) {
-    //         if (obj.command === 'send') {
-    //             // e.g. send email or pushover or whatever
-    //             this.log.info('send command');
-
-    //             // Send response in callback if required
-    //             if (obj.callback) this.sendTo(obj.from, obj.command, 'Message received', obj.callback);
-    //         }
-    //     }
-    // }
-
 }
 
 if (require.main !== module) {
